82f1f48b901fae0e1be08758788b59e46318c574
Tyler Hill <tyler@courage.local>
2018-07-10 11:26:24 -0700

    init

---
+++ nuxt.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	head: {
+		script: [
+			{ src: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js' },
+			{ src: '//cdn.jsdelivr.net/jquery.slick/1.4.1/slick.min.js' }
+		],
+		link: [
+			{rel: 'stylesheet', href: '//cdn.jsdelivr.net/jquery.slick/1.4.1/slick.css'}
+		]
+	},
+	css: [
+		'assets/cc-style.css',
+		'assets/lp.css',
+		'assets/high.css'
+
+	]
+}
---
+++ pages/index.vue
@@ -0,0 +1,44 @@
+<template>
+<div v-html="data">
+</div>
+</template>
+<script>
+let axios = require('axios');
+let getUrls = require('get-urls');
+let cheerio = require('cheerio');
+let path = require('path');
+let sanitize = require('sanitize-filename');
+let striptags = require('striptags');
+let strip = function(string) {
+	return string.replace(/[<>'":();]/gi, '');
+}
+export default {
+	async asyncData({ params }) {
+		let { data } = await axios.get('https://food.berkeley.edu');
+		const $ = cheerio.load(data);
+		let body = $('body').html();
+		let matches = body.match(/\bhttps?:\/\/\S+/gi);
+		let clean = [];
+		matches.forEach(match=>{
+			clean.push(strip(striptags(match)));
+		});
+		let imgMime = ['.jpg','.png','.gif','.svg'];
+		clean.forEach(async url=>{
+			let ext = path.extname(url);
+			if(imgMime.includes(ext)) {
+				let base = path.basename(url);
+				console.log(base);
+			}
+		});
+
+		//let urls = getUrls(data);
+		//let clean = [];
+		//urls.forEach(url=>{
+		//		clean.push(decodeURI(url));
+		//});
+		//console.log(clean);
+		
+		return { data: body }
+	}
+}
+</script>
